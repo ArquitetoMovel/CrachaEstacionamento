0b38aeb38b2fd541d35fe7450e710e6d2e77a291
Alexandre Danelon <alexandredanelon@MBPdeAlexandre.salix>
2021-05-28 21:19:50 -0300

    Novo padrão de cracha

--- Cracha.py
+++ Cracha.py
@@ -3,16 +3,11 @@ from weasyprint import HTML
 from jinja2 import Environment, FileSystemLoader
 import pandas as pd
 import numpy as np
-df = pd.read_excel("Vagas.xlsx", sheet_name="Sheet2",
-                   usecols=["Bloco", "Vaga", "Apartamento"])
-#skiprow=1
+df = pd.read_excel("Vagas2.xlsx", sheet_name="VG1",
+                   usecols=["Bloco", "Vaga", "Apartamento", "Nbloco"])
 
 env = Environment(loader=FileSystemLoader('.'))
-template = env.get_template("cracha2.html")
-
-#print (df.count())
-
-#print (df[df["Apartamento"] == 32])
+template = env.get_template("cracha3.html")
 
 html_out = "<!DOCTYPE html>"
 html_out = html_out + "<html>"
@@ -23,7 +18,8 @@ html_out = html_out + "width: 298px;"
 html_out = html_out + "height: 318px;"
 html_out = html_out + "margin-top: 15px;"
 html_out = html_out + "margin-right: 4px;"
-html_out = html_out + "background-image: url(file:///Users/alexandredanelon/Develop/CrachaEstacionamento/logo_cracha.svg);"
+html_out = html_out + \
+    "background-image: url(file:///Users/alexandredanelon/Develop/CrachaEstacionamento/logo_cracha.svg);"
 html_out = html_out + "background-position: 0 0;"
 html_out = html_out + "background-repeat: no-repeat;"
 html_out = html_out + "background-size: 95%;"
@@ -40,21 +36,20 @@ second_card = False
 last_row = len(df.index)
 
 for index, rw in df.iterrows():
-   iterator = (index + 1)
-   second_card = ((iterator % 2) == 0)
-   if second_card == False:
-      bloco1 = rw[0]
-      apto1 = rw[1]
-      Vaga1 = rw[2]
-      if iterator == last_row:
-         template_vars1 = {"bloco": bloco1, "apto": apto1, "Vaga": Vaga1}
-         html_out = html_out + template.render(template_vars1)
-   else:
-      template_vars1 = {"bloco": bloco1, "apto": apto1, "Vaga": Vaga1, "bloco2": rw[0], "apto2": rw[1], "Vaga2": rw[2]}
-      html_out = html_out + template.render(template_vars1)
-   
+    #bloco1 = rw[0]
+    id_bloco = rw[1]
+    apto1 = rw[2]
+    Vaga1 = rw[3]
+    bloco1 =rw[0]
+
+    if id_bloco == 2:
+        iterator = (index + 1)
+        template_vars1 = {"bloco": bloco1, "apto": apto1, "Vaga": Vaga1}
+        html_out = html_out + template.render(template_vars1)
+
+
 html_out = html_out + "</body>"
 html_out = html_out + "</html>"
 
-HTML(string=html_out).write_pdf("crachas.pdf")
-print(iterator , "cartões de estacionamento foram gerados.")
+HTML(string=html_out).write_pdf("crachas_bloco2.pdf")
+#print(iterator , "cartões de estacionamento foram gerados.")
